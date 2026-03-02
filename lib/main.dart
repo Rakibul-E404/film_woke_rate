4d16b5bc265ed1885ae42caea146b6728b8b7767
Rakibul-E404 <smrat246@gmail.com>
2026-03-02 16:12:54 +0600

    home UI done_

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
-      // home: const HomeScreen(),
+      // home: const SplashScreen(),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
